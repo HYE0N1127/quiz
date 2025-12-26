e85e29957690a990aba5f9552f0456f539d92da8
현빈 <hhb041127@naver.com>
2025-12-27 01:54:54 +0900

    refactor: class remove

--- page/home/index.ts
+++ page/home/index.ts
@@ -1,17 +1,10 @@
 import { SetupComponent } from "./../../component/home/setup.js";
 
-// TODO: Render 내부 구현을 바깥으로 빼기, d
-class Home {
-  constructor() {
-    this.render();
-  }
+const render = () => {
+  const root = document.querySelector("#root");
+  const setupForm = new SetupComponent();
 
-  render() {
-    const root = document.querySelector("#root");
-    const setupForm = new SetupComponent();
+  root?.append(setupForm.element);
+};
 
-    root?.append(setupForm.element);
-  }
-}
-
-export const home = new Home();
+render();
